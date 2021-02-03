558d550d4545a71f725ed32d4daad8372cbe8afb
federico29 <federico.barrios@mail.escuelaing.edu.co>
2021-02-03 09:47:31 -0500

    Pruebas ParteI punto 1 y 2

--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -31,17 +31,14 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
-
             dataSeed = dataSeed + rand.nextInt(100);
             System.out.println("Producer added " + dataSeed);
             queue.add(dataSeed);
-            
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
             }
-
         }
     }
 }
--- src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -21,20 +21,15 @@ public class StartProduction {
     public static void main(String[] args) {
         
         Queue<Integer> queue=new LinkedBlockingQueue<>();
-        
-        
+
         new Producer(queue,Long.MAX_VALUE).start();
-        
         //let the producer create products for 5 seconds (stock).
         try {
             Thread.sleep(5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(StartProduction.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
         new Consumer(queue).start();
     }
-    
-
 }
